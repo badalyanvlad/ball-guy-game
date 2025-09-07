51cedd04f71d5066b80c8e09fd3df12fd0e5f328
vladbadalyan <vladimir.badalyan@picsart.com>
2025-09-07 19:31:17 +0400

    adds game scene api

--- game_scene.h
+++ game_scene.h
@@ -3,13 +3,41 @@
 
 #include <QWidget>
 
+#include "actor.h"
+#include "ball.h"
+#include "bonus_brick.h"
+#include "brick.h"
+
 class GameScene : public QWidget
 {
     Q_OBJECT
+
+    enum class State {
+        BallsAreFlying,
+        ActorIsAiming,
+    };
 public:
     explicit GameScene(int brickCount, int brickRowCount, QWidget *parent = nullptr);
 
+    void startGame();
+
+    /**
+     * shows balls fire direction
+     */
+    void mouseMoveEvent(QMouseEvent *event) override {}
+
+    /**
+     * fires balls
+     */
+    void mouseReleaseEvent(QMouseEvent *event) override {}
+
 signals:
+
+private:
+    QVector<BonusBrick*> bonusBricks;
+    QVector<Brick*> bricks;
+    QVector<Ball*> balls;
+    Actor* actor;
 };
 
 #endif // GAME_SCENE_H
