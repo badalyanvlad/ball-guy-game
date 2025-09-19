c08b054f234f8e3d002418aedad9047d747fb22b
Ina Hovhannisyan <147070778+inaaaas@users.noreply.github.com>
2025-09-19 09:25:27 +0400

    Update bonus_brick.h
    remove bonus type

--- bonus_brick.h
+++ bonus_brick.h
@@ -7,20 +7,16 @@ class BonusBrick : public Brick
 {
     Q_OBJECT
 public:
-    enum BonusType { None, ExtraLife, ExpandPaddle, ShrinkPaddle, MultiBall, SlowBall, FastBall };
 
-    explicit BonusBrick(int health, BonusType type, QWidget *parent = nullptr);
+
+    explicit BonusBrick(int health,QWidget *parent = nullptr);
     ~BonusBrick() override;
-
-    BonusType bonusType() const;
 
     void hit() override;      // override: երբ քանդվի  bonus գցի
 
 signals:
-    void bonusDropped(BonusType type);
+    void brickCrashed();
 
-private:
-    BonusType m_type;
 };
 
 #endif // BONUSBRICK_H
