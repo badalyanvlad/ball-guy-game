f05cc425ca54fdce02e92103b9581a4e2272d0c3
Ina Hovhannisyan <147070778+inaaaas@users.noreply.github.com>
2025-09-19 09:34:48 +0400

    Update and rename bricks_manager.h to bricks_factory.h
    save only crate brick

---
+++ bricks_factory.h
@@ -0,0 +1,28 @@
+#ifndef BRICKS_FACTORY_H
+#define BRICKS_FACTORY_H
+
+#include <QVector>
+#include <QRandomGenerator>
+#include "brick.h"
+#include "bonus_brick.h"
+
+class BricksFactory : public QObject
+{
+    Q_OBJECT
+public:
+    explicit BricksFactory(QObject *parent = nullptr);
+    ~BricksFactory();
+
+    QVector<Brick*> createBricks(int numberOfBricks,
+                                 int minHealth,
+                                 int maxHealth,
+                                 bool withBonus,
+                                 QWidget *parent = nullptr);
+
+    void reset();
+
+private:
+    QVector<Brick*> m_bricks;
+};
+
+#endif // BRICKS_FACTORY_H
